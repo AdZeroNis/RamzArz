84e8b7205282e11bef3881f3e728d735f1611e32
shghygh001 <shghyghkrimi2923@gmail.com>
2023-11-14 14:17:22 +0330

    Operations performed on the project

--- Process.php
+++ Process.php
@@ -1,33 +1,32 @@
 <?php
 include ('crud.php');
-
+include('apiSMScode.php');
+$crud = new Crud();
 function generateError($message) {
     echo json_encode([
         'status' => false,
         'message' => $message
     ]);
 }
-
-$crud = new Crud();
-if(isset($_POST['users'])) {
-    $json=json_decode($_POST['users'],true);
+if(isset($_POST['signup'])) {
+    $json=json_decode($_POST['signup'],true);
     $mobile_number= $json['mobile_number'];
     $cryptocurrency_symbol = $json['cryptocurrency_symbol'];
     $price= $json['price'];
     try {
         $checkQuery = "SELECT mobile_number FROM users WHERE mobile_number= '$mobile_number'";
         $result = $crud->getConnection()->query($checkQuery)->fetchAll();
-        if (empty($mobile_number)) {
-            echo generateError("Please fill in all the required fields.");
-        } else if (count($result) > 0) {
-            echo json_encode([
-                'status' => true,
-                'message' => "already."
-            ]);
-        } else {
-            $result = $crud->execute("INSERT INTO users(mobile_number,cryptocurrency_symbol,price) VALUES ('$mobile_number','$cryptocurrency_symbol','$price')");
+        if (strlen($mobile_number)<11 || strlen($mobile_number)>11) {
+            echo generateError("Please enter the correct number.");
+        }  else {
+            $verification_code = rand(1000, 9999);
+            $result = $crud->execute("INSERT INTO users(mobile_number,cryptocurrency_symbol,price,verification_code) VALUES ('$mobile_number','$cryptocurrency_symbol','$price','$verification_code')");
+            echo "</br>";
+            sendSms($mobile_number, $verification_code);
+
         }
-    } catch (PDOException $e) {
-        echo generateError( 'Failed to connect to the database.');
+    }
+    catch (PDOException $e) {
+        echo generateError('Failed to connect to the database: ' . $e->getMessage());
     }
 }
